d1dae3779f7c282b3e253bfdb10642a6f94af487
ldvalle <34348282+ldvalle@users.noreply.github.com>
2020-07-11 12:41:15 -0300

    Add files via upload

--- src/main/resources/sql/ConsultaCorteRepo.sql
+++ src/main/resources/sql/ConsultaCorteRepo.sql
@@ -2,46 +2,54 @@ SELECT c1.numero_cliente numerosuministro,
 ' ' nroOrdenSap,
 (c1.numero_cliente || LPAD(c1.corr_corte, 4, '0') || 'CORTFOPARG') idCorteRepo,
 'C' tipoRegistro,
-c1.motivo_corte motivo,
+sm1.cod_sf1 motivo,
 CASE
 	WHEN c1.fecha_corte IS NULL THEN '20'
   ELSE '21'
 END estado,
 c1.fecha_corte fechaEjecucion,
 c1.fecha_ini_evento fechaSolicitud,
-' ' accionRealizada,
+sa1.cod_sf1 accionRealizada,
 ' ' usuario,
 ' ' tipo
-FROM correp c1
+FROM correp c1, sf_transforma sm1, OUTER (sf_transforma sa1)
 WHERE c1.numero_cliente = :#${header.numeroSuministro}
 AND c1.fecha_ini_evento BETWEEN :#${header.fechaDesde} AND :#${header.fechaHasta}
 AND c1.motivo_corte IS NOT NULL
 AND c1.tramite_repo IS NULL
+AND sm1.clave = 'MOTCORTE'
+AND sm1.cod_mac = c1.motivo_corte
+AND sa1.clave = 'ACCORTE'
+AND sa1.cod_mac = c1.accion_corte
 UNION
 SELECT c2.numero_cliente numerosuministro,
 ' ' nroOrdenSap,
 (c2.numero_cliente || LPAD(c2.corr_repo, 4, '0') || 'REPOFOPARG') idCorteRepo,
 'R' tipoRegistro,
-c2.motivo_corte motivo,
+sm2.cod_sf1 motivo,
 CASE
 	WHEN c2.fecha_sol_repo IS NULL THEN '22'
   ELSE '30'
 END estado,
 c2.fecha_reposicion fechaEjecucion,
 c2.fecha_ini_evento fechaSolicitud,
-' ' accionRealizada,
+sa2.cod_sf1 accionRealizada,
 ' ' usuario,
 ' ' tipo
-FROM correp c2
+FROM correp c2, sf_transforma sm2, OUTER (sf_transforma sa2)
 WHERE c2.numero_cliente = :#${header.numeroSuministro}
 AND c2.fecha_sol_repo BETWEEN :#${header.fechaDesde} AND :#${header.fechaHasta}
 AND c2.motivo_corte IS NOT NULL
+AND sm2.clave = 'MOTREPO'
+AND sm2.cod_mac = c2.motivo_repo
+AND sa2.clave = 'ACREPO'
+AND sa2.cod_mac = c2.accion_rehab
 UNION
 SELECT c3.numero_cliente numerosuministro,
 ' ' nroOrdenSap,
 c3.numero_cliente || TO_CHAR(c3.fecha_solicitud, '%Y%m%d') || 'PRORFOPARG' idCorteRepo,
 'P' tipoRegistro,
-c3.cod_motivo motivo,
+sm3.cod_sf1 motivo,
 CASE
 	WHEN c3.fecha_anterior + c3.dias > TODAY THEN 'A'
   ELSE 'D'
@@ -51,7 +59,9 @@ c3.fecha_solicitud fechaSolicitud,
 ' ' accionRealizada,
 c3.rol usuario,
 c3.tipo tipo
-FROM corplazo c3
+FROM corplazo c3, sf_transforma sm3
 WHERE c3.numero_cliente = :#${header.numeroSuministro}
 AND c3.fecha_solicitud BETWEEN :#${header.fechaDesde} AND :#${header.fechaHasta}
+AND sm3.clave= 'MOTPRORO'
+AND sm3.cod_mac = c3.cod_motivo
 ORDER BY 8
